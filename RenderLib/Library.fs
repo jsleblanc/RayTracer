4bb2a4c9fdfbc4a4dacabd28e169ac2a1c30fb89
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-09-21 16:50:26 -0700

    first type

--- RenderLib/Library.fs
+++ RenderLib/Library.fs
@@ -1,5 +1,15 @@
 ﻿namespace RenderLib
 
-module Say =
+module Engine =
     let hello name =
         printfn "Hello %s" name
+
+    type tuple = {
+        x: float;
+        y: float;
+        z: float;
+        w: int;
+    }
+
+    let point x y z = { x = x; y = y; z = z; w = 1; }
+    let vector x y z = { x = x; y = y; z = z; w = 0; }
