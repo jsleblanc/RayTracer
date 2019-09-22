f4dc806ba794a92cbdde03e64d651e7a2e9d95cf
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-09-21 17:17:32 -0700

    first tests passing

--- RenderLib.Tests/Library.fs
+++ RenderLib.Tests/Library.fs
@@ -1,8 +1,21 @@
 ﻿namespace RenderLib.Tests
 
 open Xunit
+open FsCheck
+open RenderLib.Engine
 
-module Say =
+module EngineTests = 
+    
     [<Fact>]
-    let hello name =
-        printfn "Hello %s" name
+    let ``point() creates tuple with w = 1``() =
+        let tupleIsPoint x y z = 
+            let t = point x y z
+            t.w = 1
+        Check.QuickThrowOnFailure tupleIsPoint
+
+    [<Fact>]
+    let ``vector() creates tuple with w = 0``() =
+        let tupleIsVector x y z = 
+            let t = vector x y z
+            t.w = 0
+        Check.QuickThrowOnFailure tupleIsVector
