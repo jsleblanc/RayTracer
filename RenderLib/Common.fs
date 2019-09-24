c0293a4976baf649de2df18174b7d5ce52fb47f1
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-09-23 22:40:57 -0700

    refactored float equality working on matrix equality

--- RenderLib/Common.fs
+++ RenderLib/Common.fs
@@ -7,7 +7,20 @@ module Common =
     [<Literal>] 
     let epsilon = 0.000001
 
-    let areEqualFloat (x:float) (y:float) =
-        (Double.IsNaN(x) && Double.IsNaN(y)) ||
-        (Double.IsInfinity(x) && Double.IsInfinity(y)) ||
-        (Math.Abs(x - y) <= epsilon)
+    let private floatEquality (a:float) (b:float) (e:float) =
+        let MinNormal = 2.2250738585072014E-308
+        let absA = Math.Abs(a)
+        let absB = Math.Abs(b)
+        let diff = Math.Abs(a - b)
+        if a.Equals(b) then 
+            true
+        else if a = 0.0 || b = 0.0 || absA + absB < MinNormal then
+            diff < (e * MinNormal)
+            else
+                diff / (absA + absB) < e
+
+    let areEqualFloat x y = floatEquality x y epsilon
+
+    let compareFloat (a:float) (b:float) = 
+        if areEqualFloat a b then 0
+        else if a < b then -1 else 1
--- RenderLib/Matrix.fs
+++ RenderLib/Matrix.fs
@@ -7,3 +7,5 @@ module Matrix =
 
     type matrix = float[,]
 
+    let matrixEquality (a:matrix) (b:matrix) = 
+        Array.compareWith compareFloat a b
