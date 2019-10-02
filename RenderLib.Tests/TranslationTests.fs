ca28a664b5ee07a377c060c26d6b8d2ba7f1752b
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-10-01 20:33:03 -0700

    more transformations - scaling

--- RenderLib.Tests/TranslationTests.fs
+++ RenderLib.Tests/TranslationTests.fs
@@ -33,3 +33,37 @@ module TranslationTests =
         let v = vector -3.0 4.0 5.0
         let r = t * v
         Assert.Equal(v, r)
+
+    [<Fact>]
+    let ``A scaling matrix applied to a point``() =
+        let t = scaling 2.0 3.0 4.0
+        let p = point -4.0 6.0 8.0
+        let r = t * p
+        let e = point -8.0 18.0 32.0
+        Assert.Equal(e, r)
+
+    [<Fact>]
+    let ``A scaling matrix applied to a vector`` () =
+        let t = scaling 2.0 3.0 4.0
+        let v = vector -4.0 6.0 8.0
+        let r = t * v
+        let e = vector -8.0 18.0 32.0
+        Assert.Equal(e, r)
+
+    [<Fact>]
+    let ``Multiplying by the inverse of a scaling matrix``() =
+        let t = scaling 2.0 3.0 4.0
+        let v = vector -4.0 6.0 8.0
+        let e = vector -2.0 2.0 2.0
+        let invTr = inverse t
+        match invTr with
+        | Ok invT -> Assert.Equal(e, invT * v)
+        | Error s -> Assert.True(false, s)
+
+    [<Fact>]
+    let ``Reflection is scaling by a negative value``() =
+        let t = scaling -1.0 1.0 1.0
+        let p = point 2.0 3.0 4.0
+        let e = point -2.0 3.0 4.0
+        let r = t * p
+        Assert.Equal(e, r)
--- RenderLib/Translations.fs
+++ RenderLib/Translations.fs
@@ -10,3 +10,10 @@ module Translations =
         i.[1,3] <- y
         i.[2,3] <- z
         i
+
+    let scaling x y z = 
+        let i = identity_matrix ()
+        i.[0,0] <- x
+        i.[1,1] <- y
+        i.[2,2] <- z
+        i
