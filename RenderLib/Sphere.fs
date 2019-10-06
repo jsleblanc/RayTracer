0d37e95f7b41be09ccee1370a87487987c99e44b
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-10-05 20:31:38 -0700

    sphere intersection

--- RenderLib/Sphere.fs
+++ RenderLib/Sphere.fs
@@ -3,6 +3,7 @@
 open System
 open Common
 open Tuple
+open Matrix
 open Ray
 
 module Sphere = 
@@ -12,4 +13,14 @@ module Sphere =
         new() = sphere(Guid.NewGuid())
 
     let intersect s r =
-        [|1.0;2.0;3.0|]
+        let sphereToRay = r.origin - point 0.0 0.0 0.0
+        let a = r.direction.dotProduct(r.direction)
+        let b = 2.0 * r.direction.dotProduct(sphereToRay)
+        let c = sphereToRay.dotProduct(sphereToRay) - 1.0
+        let discriminant = b**2.0 - 4.0 * a * c
+        if discriminant < 0.0 then
+            List.empty<float>
+        else 
+            let t1 = (-b - Math.Sqrt(discriminant)) / (2.0 * a)
+            let t2 = (-b + Math.Sqrt(discriminant)) / (2.0 * a)
+            [ t1; t2; ]
