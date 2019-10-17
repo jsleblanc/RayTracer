e3b98675c2e405c087db5c1ac350980ff27f8761
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-10-16 21:57:03 -0700

    cameras

---
+++ RenderLib.Tests/CameraTests.fs
@@ -0,0 +1,67 @@
+﻿namespace RenderLib.Tests
+
+open Xunit
+open FsCheck
+open System
+open RenderLib.Common
+open RenderLib.Tuple
+open RenderLib.Matrix
+open RenderLib.Color
+open RenderLib.Translations
+open RenderLib.Lights
+open RenderLib.Shapes
+open RenderLib.Worlds
+open RenderLib.Camera
+
+module CameraTests = 
+
+    let default_world = { world.Default with objs = [Sphere({ shapeProperties.Default with material = { material.Default with color = color 0.8 1.0 0.6; diffuse = 0.7; specular = 0.2; }}); Sphere({ shapeProperties.Default with default_transformation = scaling 0.5 0.5 0.5; })]; }
+
+    [<Fact>]
+    let ``Constructing a camera``() =
+        let c = create_default_camera 160 120
+        Assert.Equal(160, c.hsize)
+        Assert.Equal(120, c.vsize)
+        Assert.Equal(Math.PI / 2.0, c.field_of_view)
+        Assert.Equal(identity_matrix (), c.transform)
+
+    [<Fact>]
+    let ``The pixel size for a horizontal canvas``() =
+        let c = create_default_camera 200 125
+        Assert.True(areEqualFloat 0.01 c.pixel_size)
+
+    [<Fact>]
+    let ``The pixel size for a vertical canvas``() =
+        let c = create_default_camera 125 200
+        Assert.True(areEqualFloat 0.01 c.pixel_size)
+
+    [<Fact>]
+    let ``Constructing a ray through the center of the canvas``() =
+        let c = create_default_camera 201 101
+        let r = ray_for_pixel c 100 50
+        Assert.Equal(point 0.0 0.0 0.0, r.origin)
+        Assert.Equal(vector 0.0 0.0 -1.0, r.direction)
+
+    [<Fact>]
+    let ``Constructing a ray through a corner of the canvas``() =
+        let c = create_default_camera 201 101
+        let r = ray_for_pixel c 0 0
+        Assert.Equal(point 0.0 0.0 0.0, r.origin)
+        Assert.Equal(vector 0.6651864261 0.3325932131 -0.6685123583, r.direction)
+
+    [<Fact>]
+    let ``Constructing a ray when the camera is transformed``() =
+        let c = { create_default_camera 201 101 with transform = rotation_y (Math.PI/4.0) * translation 0.0 -2.0 5.0; }
+        let r = ray_for_pixel c 100 50
+        Assert.Equal(point 0.0 2.0 -5.0, r.origin)
+        Assert.Equal(vector (Math.Sqrt(2.0)/2.0) 0.0 (-Math.Sqrt(2.0)/2.0), r.direction)
+
+    [<Fact>]
+    let ``Rendering a world with a camera``() =
+        let w = default_world        
+        let from_point = point 0.0 0.0 -5.0
+        let to_point = point 0.0 0.0 0.0
+        let up = vector 0.0 1.0 0.0
+        let c = { create_default_camera 11 11 with transform = view_transform from_point to_point up; }
+        let image = render c w
+        Assert.Equal(image.[5,5], color 0.38066 0.47583 0.2855)
---
+++ RenderLib/Camera.fs
@@ -0,0 +1,77 @@
+﻿namespace RenderLib
+
+open System
+open Canvas
+open Matrix
+open Tuple
+open Ray
+open Worlds
+
+module Camera = 
+
+    type camera = {
+        hsize: int;
+        vsize: int;
+        field_of_view: float;
+        transform: matrix
+        pixel_size: float;
+        half_width: float;
+        half_height: float;
+    } 
+
+    let private calculate hsize vsize field_of_view =
+        let half_view = Math.Tan (field_of_view / 2.0)
+        let aspect = (float) hsize / (float) vsize
+        let mutable half_width = 0.0
+        let mutable half_height = 0.0
+        if aspect >= 1.0 then
+            half_width <- half_view
+            half_height <- half_view / aspect
+        else
+            half_width <- half_view * aspect
+            half_height <- half_view
+        let pixel_size = (half_width * 2.0) / (float) hsize
+        (half_width, half_height, pixel_size)
+
+    
+    let create_camera hsize vsize field_of_view =
+        let (hw, hh, ps) = calculate hsize vsize field_of_view
+        {
+            hsize = hsize;
+            vsize = vsize;
+            field_of_view = field_of_view;
+            transform = identity_matrix ();
+            pixel_size = ps;
+            half_height = hh;
+            half_width = hw;
+        }
+
+    let create_default_camera hsize vsize =
+        create_camera hsize vsize (Math.PI / 2.0)
+
+    let ray_for_pixel camera (px:int) (py:int) =
+        let xoffset = ((float)px + 0.5) * camera.pixel_size
+        let yoffset = ((float)py + 0.5) * camera.pixel_size
+        let world_x = camera.half_width - xoffset
+        let world_y = camera.half_height - yoffset
+        match inverse camera.transform with
+        | Ok ict -> 
+            let pixel = ict * (point world_x world_y -1.0)
+            let origin = ict * (point 0.0 0.0 0.0)
+            let direction = (pixel - origin).normalize()
+            {
+                origin = origin;
+                direction = direction;
+            }
+        | Error s -> raise (Exception(s)) //TODO - decide how I want this to propagate through the code; exception is temporary
+
+    let render camera world =
+        let image = create_canvas camera.hsize camera.vsize
+        let coords = seq {
+            for y in 0 .. camera.vsize - 1 do
+                for x in 0 .. camera.hsize - 1 do
+                    yield (x,y)
+            }
+        coords
+        |> Seq.toList
+        |> PSeq.map
