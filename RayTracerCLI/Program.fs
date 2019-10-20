512f8777bbb91236f0a6a0556f915826bbd25269
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-10-20 13:19:19 -0700

    scene changes

--- RayTracerCLI/Program.fs
+++ RayTracerCLI/Program.fs
@@ -21,6 +21,7 @@ let main argv =
     let red = color 1.0 0.0 0.0
     let blue = color 0.0 0.0 1.0
     let green = color 0.0 1.0 0.0
+    let yellow = color 1.0 1.0 0.0
 
     let canvas_to_jpg (c:Canvas.canvas) =
         let encoder = new JpegEncoder()
@@ -38,7 +39,7 @@ let main argv =
 
     //let p = Solid(red)// blue
     //let p = checkers_pattern (scaling 0.5 0.5 0.5) red blue
-    let p = Blended(stripe_pattern_default red blue, stripe_pattern (rotation_y(-Math.PI/2.0)) red blue)
+    let p = Blended(stripe_pattern_default white black, stripe_pattern (rotation_y(-Math.PI/2.0)) white black)
     let planeMaterial = 
         { material.Default with color = color 1.0 0.9 0.9; specular = 0.0; pattern = Some p; }
 
@@ -47,22 +48,22 @@ let main argv =
         //Plane({ shapeProperties.Default with material = planeMaterial; default_transformation = (translation 0.0 0.0 10.0) * (rotation_x (Math.PI/2.0)); })
 
     let middle = 
-        let m = { material.Default with color = color 0.1 1.0 0.5; diffuse = 0.7; specular = 0.3; reflective = 1.0; }
+        let m = { material.Default with color = blue; diffuse = 0.9; specular = 0.3; reflective = 1.0; }
         Sphere({ shapeProperties.Default with material = m; default_transformation = translation -0.5 1.0 0.5; })
 
     let right =
-        let m = { material.Default with color = color 0.5 1.0 0.1; diffuse = 0.7; specular = 0.3; }
+        let m = { material.Default with color = red; diffuse = 0.7; specular = 0.3; }
         Sphere({ shapeProperties.Default with material = m; default_transformation = (translation 1.5 0.5 -0.5) * (scaling 0.5 0.5 0.5); })
 
     let left = 
-        let m = { material.Default with color = color 1.0 0.8 0.1; diffuse = 0.7; specular = 0.3; }
+        let m = { material.Default with color = yellow; diffuse = 0.7; specular = 0.3; }
         Sphere({ shapeProperties.Default with material = m; default_transformation = (translation -1.5 0.33 -0.75) * (scaling 0.33 0.33 0.33); })
 
     //let light = { position = point 0.0 10.0 -10.0; intensity = color 1.0 1.0 1.0; }
     let world = { world.Default with objs = [ plane; middle; right; left; ]; }
 
     let vt = view_transform (point 0.0 1.5 -5.0) (point 0.0 1.0 0.0) (vector 0.0 1.0 0.0)
-    let camera = { create_default_camera 640 480 with transform = vt; }
+    let camera = { create_default_camera 1920 1200 with transform = vt; }
 
     printfn "Calculating..."
     let sw = Stopwatch.StartNew()
