6140ab8f552115d427708495fabe53fe950fb48f
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-10-23 19:39:37 -0700

    scene

--- RayTracerCLI/Program.fs
+++ RayTracerCLI/Program.fs
@@ -39,7 +39,7 @@ let main argv =
 
     //let p = Solid(red)// blue
     //let p = checkers_pattern (scaling 0.5 0.5 0.5) red blue
-    let p = Blended(stripe_pattern_default white black, stripe_pattern (rotation_y(-Math.PI/2.0)) white black)
+    let p = Blended(stripe_pattern_default white blue, stripe_pattern (rotation_y(-Math.PI/2.0)) white blue)
     let planeMaterial = 
         { material.Default with color = color 1.0 0.9 0.9; specular = 0.0; pattern = Some p; }
 
@@ -48,12 +48,12 @@ let main argv =
         //Plane({ shapeProperties.Default with material = planeMaterial; default_transformation = (translation 0.0 0.0 10.0) * (rotation_x (Math.PI/2.0)); })
 
     let middle = 
-        let m = { material.Default with color = blue; diffuse = 0.9; specular = 0.3; reflective = 1.0; }
+        let m = { glass with diffuse = 0.01; ambient = 0.2; reflective = 0.9; specular = 1.0; shininess = 300.0; }
         Sphere({ shapeProperties.Default with material = m; default_transformation = translation -0.5 1.0 0.5; })
 
     let right =
         let m = { material.Default with color = red; diffuse = 0.7; specular = 0.3; }
-        Sphere({ shapeProperties.Default with material = m; default_transformation = (translation 1.5 0.5 -0.5) * (scaling 0.5 0.5 0.5); })
+        Sphere({ shapeProperties.Default with material = m; default_transformation = (translation 0.75 0.5 5.5) * (scaling 0.75 0.75 0.75); })
 
     let left = 
         let m = { material.Default with color = yellow; diffuse = 0.7; specular = 0.3; }
