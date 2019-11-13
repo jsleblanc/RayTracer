3564d2ae67f0ab06726b457284ad8aadfef2d9a6
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-11-12 18:05:10 -0800

    diagnosing issues with calculating normals when shapes are in groups

--- RayTracerCLI/Program.fs
+++ RayTracerCLI/Program.fs
@@ -47,85 +47,80 @@ let main argv =
         Cylinder(material.Default,(translation 0.0 0.0 -1.0) * (rotation_y (-Math.PI/6.0)) * (rotation_z (-Math.PI/2.0)) * (scaling 0.25 1.0 0.25),None,0.0,1.0,false)
 
     let hexagon_side t =
-        let side = Group(None,t,None,new HashSet<shape>())
+        let side = Group(Some material.Default,t,None,new HashSet<shape>())
         side
         |> with_child hexagon_corner
         |> with_child hexagon_edge
 
-    let hexagon t = 
+    let hexagon t =         
         let hex = Group(None,t,None,new HashSet<shape>())
         for x in 0 .. 5 do
-            let v = float x
-            let side = hexagon_side (rotation_y (v*Math.PI/3.0))
+            let side = hexagon_side (rotation_y (float x*Math.PI/3.0))
             with_child side hex
         hex
 
-    let g = Group(None,identity_matrix(),None,new HashSet<shape>())
+    let hexagon2 t = 
+        //let t = scaling 1.5 1.5 1.5
+        let children = new HashSet<shape>()
+        let hex = Group(Some material.Default,t,None,children)
 
-    //let p = Solid(red)// blue
-    let p = checkers_pattern (scaling 0.5 0.5 0.5) white blue
-    //let p = Blended(stripe_pattern_default white blue, stripe_pattern (rotation_y(-Math.PI/2.0)) white blue)
-    let planeMaterial = 
-        { material.Default with color = color 1.0 0.9 0.9; specular = 0.0; pattern = Some p; }
+        let children1 = new HashSet<shape>()
+        let side1 = Group(Some material.Default,(rotation_y (0.0*Math.PI/3.0)) * t,None,children1)
+        let s1 = Sphere(material.Default,(translation 0.0 0.0 -1.0) * (scaling 0.25 0.25 0.25),Some side1)
+        let c1 = Cylinder(material.Default,(translation 0.0 0.0 -1.0) * (rotation_y (-Math.PI/6.0)) * (rotation_z (-Math.PI/2.0)) * (scaling 0.25 1.0 0.25),Some side1,0.0,1.0,false)
+        children1.Add(s1)
+        children1.Add(c1)
 
-    let plane = Plane(planeMaterial,identity_matrix(),None)
-        //Plane({ shapeProperties.Default with material = planeMaterial; default_transformation = (translation 0.0 0.0 10.0) * (rotation_x (Math.PI/2.0)); })
+        let children2 = new HashSet<shape>()
+        let side2 = Group(Some material.Default,(rotation_y (1.0*Math.PI/3.0)) * t,None,children2)
+        let s2 = Sphere(material.Default,(translation 0.0 0.0 -1.0) * (scaling 0.25 0.25 0.25) * (rotation_y (1.0*Math.PI/3.0)),Some side2)
+        let c2 = Cylinder(material.Default,(translation 0.0 0.0 -1.0) * (rotation_y (-Math.PI/6.0)) * (rotation_z (-Math.PI/2.0)) * (scaling 0.25 1.0 0.25) * (rotation_y (1.0*Math.PI/3.0)),Some side2,0.0,1.0,false)
+        children2.Add(s2)
+        children2.Add(c2)
 
-    let middle = 
-        let m = { glass with diffuse = 0.01; ambient = 0.02; reflective = 0.9; specular = 1.0; shininess = 300.0; }
-        Sphere(m,translation -0.5 1.0 0.5,None)
-
-    let right =
-        let m = { material.Default with color = red; diffuse = 0.7; specular = 0.3; }
-        Sphere(m,(translation -0.75 1.5 5.0) * (scaling 0.75 0.75 0.75),None)
-
-    let left = 
-        let m = { material.Default with color = yellow; diffuse = 0.7; specular = 0.3; }
-        Sphere(m,(translation -1.5 0.33 -0.75) * (scaling 0.33 0.33 0.33),None)
-
-    let cube =
-        Cube({ material.Default with color = green; },(translation -3.5 1.0 0.5) * (scaling 0.75 0.75 0.75) * (rotation_y (Math.PI/3.5)),None)
-
-    let cylinder =
-        Cylinder({ material.Default with color = green; },(translation -3.5 1.0 0.5) * (scaling 0.75 0.75 0.75) * (rotation_y (Math.PI/3.5)),None,1.0,3.0,true)
-
-    //with_child g plane
-    with_child middle g
-    with_child right g
-    with_child left g
-    with_child cube g
-    with_child cylinder g
+        children.Add(side1)
+        //children.Add(side2)
+        hex
 
     //((rotation_z (Math.PI / -3.0)) * (rotation_x (Math.PI/2.0)))
     
-    let vt = view_transform (point 0.0 1.5 -3.0) (point 0.0 1.0 0.0) (vector 0.0 1.0 0.0)    
+    let vt = view_transform (point 0.0 0.0 -3.0) (point 0.0 1.0 0.0) (vector 0.0 1.0 0.0)    
     let camera = { create_default_camera 640 480 with transform = vt; }
     let light = { position = point 0.0 10.0 -10.0; intensity = color 1.0 1.0 1.0; }
-        
-    (*
-    //sphere inside a sphere 
-    let pt = checkers_pattern (translation 0.0 0.1 0.0) blue white
-    let plane = Plane({ material.Default with pattern = Some pt; },translation 0.0 -10.1 0.0,None)
-    let s1 = Sphere({glass with diffuse = 0.1; shininess = 300.0; reflective = 1.0; },scaling 1.25 1.25 1.25,None)
-    let s2 = Sphere({glass with diffuse = 0.1; shininess = 300.0; reflective = 1.0; refractive_index = 1.0;},scaling 0.75 0.75 0.75,None)
-    let light = { position = point 20.0 10.0 0.0; intensity = color 0.7 0.7 0.7; }
-    let vt = view_transform (point 0.0 2.5 0.0) (point 0.0 0.0 0.0) (vector 1.0 0.0 0.0)
-    let camera = { create_default_camera 3840 2160 with field_of_view = Math.PI / 3.0; transform = vt; }
-    let world = { world.Default with light = light; objs = [plane;s1;s2;];}
-    *)
+    
+    let us = Sphere(material.Default,(rotation_z (Math.PI/4.0))*(rotation_z (Math.PI/3.0))*(rotation_z (Math.PI/3.0))*(scaling 1.0 0.5 0.5),None)
+
+    let sr = Sphere({ material.Default with color = blue; },identity_matrix(),None)
+    let g1 = Group(None,(rotation_z (Math.PI/3.0)),None,new HashSet<shape>())
+    let s = Sphere(material.Default,(rotation_z (Math.PI/3.0))*(scaling 1.0 0.5 0.5),Some g1)
+    //let s = Cylinder(material.Default,(scaling 1.0 0.5 0.5),Some g1,0.0,1.0,false)
+    with_child s g1
+    let g2 = Group(None,(rotation_z (Math.PI/4.0)),Some g1,new HashSet<shape>())
+    with_child g1 g2
+    let world = { world.Default with light = light; objs = [g2;]; }    
+    let canvas = render camera world
+    canvas_to_jpg "output.jpg" canvas
 
     printfn "Calculating..."
     let sw = Stopwatch.StartNew()   
-
+    (*
     let radians = Math.PI / 180.0
-    let mutable r = 1.0 * radians
-    for x in 1 .. 360 do
-        let world = { world.Default with light = light; objs = [ hexagon (rotation_x r); ]; }    
-        let canvas = render camera world
-        let filename = sprintf "Test_%03i.jpg" x
-        canvas_to_jpg filename canvas
-        printfn "%s done in %s" filename (sw.Elapsed.ToString())
-        r <- float x * radians
+    let mutable r = 53.0 * radians
+    //for x in 1 .. 360 do
+    let x = 53
+    let world = { world.Default with light = light; objs = [ hexagon (rotation_x r); ]; }    
+    let canvas = render camera world
+    let filename = sprintf "Test_%03i.jpg" x
+    canvas_to_jpg filename canvas
+    printfn "%s done in %s" filename (sw.Elapsed.ToString())
+    r <- float x * radians
+  *)
+
+    //let corner_transform = (translation 0.0 0.0 -1.0) * (scaling 0.25 0.25 0.25)// * (rotation_x r)
+    //let edge_transform = (translation 0.0 0.0 -1.0) * (rotation_y (-Math.PI/6.0)) * (rotation_z (-Math.PI/2.0)) * (scaling 0.25 1.0 0.25)// * (rotation_x r)
+
+    //let ray = ray_for_pixel camera 320 254
+    //let color = color_at world ray 5
 
     //let canvas = render camera world
 
