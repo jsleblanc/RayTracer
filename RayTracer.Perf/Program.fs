be2dbbee373ab986f3502a10685a832e4c5c7592
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-11-16 23:42:17 -0800

    first benchmark

--- RayTracer.Perf/Program.fs
+++ RayTracer.Perf/Program.fs
@@ -1,43 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open BenchmarkDotNet.Running
+open RayTracer.Perf.RayIntersections
 
-open System
-open System.IO
-open System.Collections.Concurrent
-open BenchmarkDotNet.Attributes
-open BenchmarkDotNet.Running
-
-let getStrings len = Array.init len (fun _ -> Path.GetRandomFileName())
-
-let lookup arr (dict:ConcurrentDictionary<string,bool>) =
-    arr |> Array.iteri(fun idx elm -> 
-        let mutable b = dict.[elm]
-        b <- dict.[arr.[0]])
-
-
-type StringKeyComparison () =
-    let mutable arr : string [] = [||]
-    let dict1 = ConcurrentDictionary<_,_>()
-    let dict2 = ConcurrentDictionary<_,_>(StringComparer.Ordinal)
-
-    [<Params (100, 500, 1000, 2000)>] 
-    member val public DictSize = 0 with get, set
-
-    [<GlobalSetup>]
-    member self.GlobalSetupData() =
-        dict1.Clear(); dict2.Clear()
-        arr <- getStrings self.DictSize
-        arr |> Array.iter (fun x -> dict1.[x] <- true ; dict2.[x] <- true)
-
-    [<Benchmark>]
-    member self.StandardLookup () = lookup arr dict1
-
-    [<Benchmark>]
-    member self.OrdinalLookup () = lookup arr dict2
-
-let defaultSwitch () = BenchmarkSwitcher [|typeof<StringKeyComparison>;|]
+let defaultSwitch () = BenchmarkSwitcher [|typeof<RayIntersectionBenchmarks>;|]
 
 [<EntryPoint>]
 let main argv =
---
+++ RayTracer.Perf/RayIntersections.fs
@@ -0,0 +1,38 @@
+﻿namespace RayTracer.Perf
+
+open RenderLib
+open RenderLib.Tuple
+open RenderLib.Material
+open RenderLib.Translations
+open RenderLib.Color
+open RenderLib.Worlds
+open RenderLib.Ray
+open BenchmarkDotNet.Attributes
+
+module RayIntersections =
+
+    type RayIntersectionBenchmarks () =
+        let default_world = 
+            let s1 = 
+                ShapeSphere.build
+                |> Shapes2.texture { Material.material.Default with color = color 0.8 1.0 0.6; diffuse = 0.7; specular = 0.2; }
+            let s2 = 
+                ShapeSphere.build
+                |> Shapes2.texture Material.material.Default
+                |> Shapes2.transform (scaling 0.5 0.5 0.5)
+            let w = Worlds.build_default [s1; s2;]
+            (s1,s2,w)
+        let mutable world = Worlds.build_default []
+
+        [<GlobalSetup>]
+        member self.GlobalSetup() =
+            let (_,_,w) = default_world
+            world <- w
+
+        [<Benchmark>]
+        member self.IntersectWorldWithRay () =
+            let r = {
+                origin = point 0.0 0.0 -5.0;
+                direction = vector 0.0 0.0 1.0;
+            }
+            intersect_world world r
