a3ad6ffb00157939082db00a0713bd8b2793f0aa
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-11-16 22:38:52 -0800

    group tests refactored

--- RenderLib.Tests/GroupTests.fs
+++ RenderLib.Tests/GroupTests.fs
@@ -1,6 +1,7 @@
 ﻿namespace RenderLib.Tests
 
 open Xunit
+open System
 open RenderLib
 open RenderLib.Tuple
 open RenderLib.Translations
@@ -55,32 +56,43 @@ module GroupTests =
         Assert.Equal(2, Seq.length xs)
 
     [<Fact>]
-    let ``Converting a point from world to object space``() =
-        let g1 = Group(Some material.Default,rotation_y (Math.PI/2.0),None,new HashSet<shape>())
-        let g2 = Group(Some material.Default,scaling 2.0 2.0 2.0,Some g1,new HashSet<shape>())
-        add_child g1 g2
-        let s = Sphere(material.Default,translation 5.0 0.0 0.0,Some g2)
-        add_child g2 s
-        let p = world_to_object s (point -2.0 0.0 -10.0)
+    let ``Converting a point from world to object space``() =        
+        let s = ShapeSphere.build |> Shapes2.transform (translation 5.0 0.0 0.0)
+        let g2 = ShapeGroup.build [s;] |> Shapes2.transform (scaling 2.0 2.0 2.0)        
+        let g1 = ShapeGroup.build [g2;] |> Shapes2.transform (rotation_y (Math.PI/2.0))
+        let p = world_to_object s [g2;g1;] (point -2.0 0.0 -10.0)
         Assert.Equal(point 0.0 0.0 -1.0, p)
 
     [<Fact>]
     let ``Converting a normal from object to world space``() =
-        let g1 = Group(Some material.Default,rotation_y (Math.PI/2.0),None,new HashSet<shape>())
-        let g2 = Group(Some material.Default,scaling 1.0 2.0 3.0,Some g1,new HashSet<shape>())
-        add_child g1 g2
-        let s = Sphere(material.Default,translation 5.0 0.0 0.0,Some g2)
-        add_child g2 s
+        let s = ShapeSphere.build |> Shapes2.transform (translation 5.0 0.0 0.0)
+        let g2 = ShapeGroup.build [s;] |> Shapes2.transform (scaling 1.0 2.0 3.0)
+        let g1 = ShapeGroup.build [g2;] |> Shapes2.transform (rotation_y (Math.PI/2.0))
         let q = (Math.Sqrt(3.0)/3.0) 
-        let n = normal_to_world s (vector q q q)
+        let n = normal_to_world s [g2;g1;] (vector q q q)
         Assert.Equal(vector 0.2857142857 0.4285714286 -0.8571428571, n)
 
     [<Fact>]
     let ``Finding the nomral on a child object``() =
-        let g1 = Group(Some material.Default,rotation_y (Math.PI/2.0),None,new HashSet<shape>())
-        let g2 = Group(Some material.Default,scaling 1.0 2.0 3.0,Some g1,new HashSet<shape>())
-        add_child g1 g2
-        let s = Sphere(material.Default,translation 5.0 0.0 0.0,Some g2)
-        add_child g2 s
-        let n = normal_at s (point 1.7321 1.1547 -5.5774)
+        let s = ShapeSphere.build |> Shapes2.transform (translation 5.0 0.0 0.0)
+        let g2 = ShapeGroup.build [s;] |> Shapes2.transform (scaling 1.0 2.0 3.0)
+        let g1 = ShapeGroup.build [g2;] |> Shapes2.transform (rotation_y (Math.PI/2.0))
+        let world_p = point 1.7321 1.1547 -5.5774
+        let n = normal_at None s [g2;g1;] world_p
         Assert.Equal(vector 0.2857036818 0.4285431518 -0.8571605294, n)
+
+    [<Fact>]
+    let ``Converting a point from world to object space nested groups``() =
+        let s = ShapeSphere.build |> Shapes2.transform ((rotation_z (Math.PI/3.0))*(scaling 1.0 0.5 0.5))
+        let g2 = ShapeGroup.build [s;] |> Shapes2.transform (rotation_z (Math.PI/4.0))
+        let g1 = ShapeGroup.build [g2;] |> Shapes2.transform (rotation_z (Math.PI/3.0))        
+        let p = world_to_object s [g2;g1;] (point -0.4975074385 -0.2721372954 -0.2336173106)
+        Assert.Equal(point 0.4101209687 0.7832576841 -0.4672346213, p)
+
+    [<Fact>]
+    let ``Converting a normal from object to world space nested groups``() =
+        let s = ShapeSphere.build |> Shapes2.transform ((rotation_z (Math.PI/3.0))*(scaling 1.0 0.5 0.5))
+        let g1 = ShapeGroup.build [s;] |> Shapes2.transform (rotation_z (Math.PI/3.0))
+        let g2 = ShapeGroup.build [g1;] |> Shapes2.transform (rotation_z (Math.PI/4.0))
+        let v = normal_to_world s [g2;g1;] (vector 0.4101209687 0.7832576841 -0.4672346213)
+        Assert.Equal(vector -0.428749814 -0.7525625233 -0.4998232143, v)
