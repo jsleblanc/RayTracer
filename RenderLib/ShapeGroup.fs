4aa24d5637bb1dad86a41aa68c5fbdc5566e2a81
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-11-17 17:42:59 -0800

    implemented bounding box checking on group removed old shapes code

--- RenderLib/ShapeGroup.fs
+++ RenderLib/ShapeGroup.fs
@@ -17,13 +17,15 @@ module ShapeGroup =
 
     let build children = 
         let local_intersect shape trail ray =
-            let rec loop xs children =
-                match children with
-                | [] -> xs
-                | child :: children ->
-                    let xs_p = Shapes2.intersect child (shape :: trail) ray
-                    loop (List.append xs_p xs) children
-            Shapes2.sort_intersection (loop [] (get_children shape))
+            if BoundingBoxes.intersects (shape.bounds_of shape) ray then
+                let rec loop xs children =
+                    match children with
+                    | [] -> xs
+                    | child :: children ->
+                        let xs_p = Shapes2.intersect child (shape :: trail) ray
+                        loop (List.append xs_p xs) children
+                Shapes2.sort_intersection (loop [] (get_children shape))
+            else []
         let local_normal_at hit shape pt = raise (Exception "Groups do not have normals!")
         let bounds_of shape = 
             let func box shape = BoundingBoxes.add_boxes box (parent_space_bounds_of shape)
