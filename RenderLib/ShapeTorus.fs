750b955280d896ffd0ec3a88d6f70c6b79b30eff
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-12-14 18:27:27 -0800

    rewriting torus, but still not working

--- RenderLib/ShapeTorus.fs
+++ RenderLib/ShapeTorus.fs
@@ -11,8 +11,20 @@ open Shapes
 
 module ShapeTorus = 
 
-    let cbrt a n = 
-        let twopi = 2.0 * Math.PI
+    let twopi = 2.0 * Math.PI
+    let cpx2 = new Complex(2.0,0.0)
+    let cpx3 = new Complex(2.0,0.0)
+    let cpx4 = new Complex(4.0,0.0)
+    let cpx5 = new Complex(5.0,0.0)
+    let cpx6 = new Complex(6.0,0.0)
+    let cpx8 = new Complex(8.0,0.0)
+    let cpx12 = new Complex(12.0,0.0)
+    let cpx16 = new Complex(16.0,0.0)
+    let cpx27 = new Complex(27.0,0.0)
+    let cpx108 = new Complex(108.0,0.0)
+    let cpx256 = new Complex(256.0,0.0)
+
+    let cbrt a n =         
         let rho = Math.Pow(Complex.Abs(a), 1.0/3.0)
         let theta = ((twopi * float n) + a.Phase) / 3.0
         new Complex(rho * Math.Cos(theta), rho * Math.Sin(theta))
@@ -24,14 +36,12 @@ module ShapeTorus =
             else
                 [-c / b]
         else
-            let c2 = new Complex(2.0,0.0)
-            let c4 = new Complex(4.0,0.0)
-            let radicand = b*b - c4*a*c
+            let radicand = b*b - cpx4*a*c
             if radicand = Complex.Zero then
-                [-b / (c2 * a)]
+                [-b / (cpx2 * a)]
             else
                 let r = Complex.Sqrt(radicand)
-                let d = c2 * a
+                let d = cpx2 * a
                 [
                     (-b + r) / d;
                     (-b - r) / d;
@@ -41,15 +51,13 @@ module ShapeTorus =
         if a = Complex.Zero then
             solveQuadratic a b c
         else
-            let c2 = new Complex(2.0,0.0)
-            let c3 = new Complex(3.0,0.0)
             let b = b / a
             let c = c / a
             let d = d / a
 
-            let S = b / c3
-            let D = c / c3 - S*S
-            let E = S*S*S + (d - S*c)/c2
+            let S = b / cpx3
+            let D = c / cpx3 - S*S
+            let E = S*S*S + (d - S*c)/cpx2
             let Froot = Complex.Sqrt(E*E + D*D*D)
             let mutable F = -Froot - E
             if F = Complex.Zero then
@@ -60,148 +68,91 @@ module ShapeTorus =
             [0..3]
             |> List.map func
 
-    let private solve2 (coefficients:float list) = 
-        let p = coefficients.[1] / (2.0 * coefficients.[2])
-        let q = coefficients.[0] / coefficients.[2]
-        let d = p * p - q
-        if areEqualFloat d 0.0 then
-            [-p]
+    let solveQuartic a b c d e =
+        if a = Complex.Zero then
+            solveCubic b c d e
         else
-            if d < 0.0 then
-                []
+            let b = b / a
+            let c = c / a
+            let d = d / a
+            let e = e / a
+            let b2 = b*b
+            let b3 = b*b2
+            let b4 = b2*b2
+            let alpha = (-cpx3/cpx8)*b2+c
+            let beta = b3/cpx8 - b*c/cpx2 + d
+            let gamma = (-cpx3/cpx256)*b4 + b2*c/cpx16 - b*d/cpx4 + e
+            let alpha2 = alpha * alpha
+            let t = -b / cpx4
+            if beta = Complex.Zero then
+                let rad = Complex.Sqrt(alpha2 - cpx4/gamma)
+                let r1 = Complex.Sqrt((-alpha + rad) / cpx2)
+                let r2 = Complex.Sqrt((-alpha - rad) / cpx2)
+                [
+                    t + r1;
+                    t - r1;
+                    t + r2;
+                    t - r2;
+                ]
             else
-                let sqrt_d = Math.Sqrt(d)
-                [sqrt_d - p;-sqrt_d - p;]
-
-
-    let private solve3 (coefficients:float list) = 
-        let a = coefficients.[2] / coefficients.[3]
-        let b = coefficients.[1] / coefficients.[3]
-        let c = coefficients.[0] / coefficients.[3]
-        let sq_a = a**2.0
-        let p = 1.0 / 3.0 * (-1.0 / 3.0 * sq_a + b)
-        let q = 1.0 / 2.0 * (2.0 / 27.0 * a * sq_a - 1.0 / 3.0 * a * b + c)
-        let cb_p = p**3.0
-        let d = q * q + cb_p
-        let s =
-            if areEqualFloat d 0.0 then
-                if areEqualFloat q 0.0 then
-                    [0.0]
+                let alpha3 = alpha * alpha2
+                let P = -(alpha2/cpx12 + gamma)
+                let Q = -alpha3/cpx108 + alpha*gamma/cpx3 - beta*beta/cpx8
+                let R = -Q/cpx2 + Complex.Sqrt(Q*Q/cpx4 + P*P*P/cpx27)
+                let U = cbrt R 0
+                let mutable y = (-cpx5/cpx6)*alpha + U
+                if U = Complex.Zero then
+                    y <- y - cbrt Q 0
                 else
-                    let u = Math.Cbrt(-q)
-                    [2.0 * u; -u]
-            else
-                if d < 0.0 then
-                    let phi = 1.0 / 3.0 * Math.Acos(-q / Math.Sqrt(-cb_p))
-                    let t = 2.0 * Math.Sqrt(-p)
-                    [
-                        t * Math.Cos(phi);
-                        -t * Math.Cos(phi + Math.PI / 3.0);
-                        -t * Math.Cos(phi - Math.PI / 3.0);
-                    ]
-                else
-                    let sqrt_d = Math.Sqrt(d)
-                    let u = Math.Cbrt(sqrt_d - q)
-                    let v = -Math.Cbrt(sqrt_d + q)
-                    [u + v]
-        let sub = 1.0 / 3.0 * a
-        s |> List.map (fun t -> t - sub)
+                    y <- y - P/(cpx3 * U)
+                let W = Complex.Sqrt(alpha + cpx2*y)
+                let r1 = Complex.Sqrt(-(cpx3*alpha + cpx2*y + cpx2*beta/W))
+                let r2 = Complex.Sqrt(-(cpx3*alpha + cpx2*y - cpx2*beta/W))
+                [
+                    t + (W - r1)/cpx2;
+                    t + (W + r1)/cpx2;
+                    t + (-W - r2)/cpx2;
+                    t + (-W + r2)/cpx2;
+                ]
 
-    let private solve4 (coefficients:float list) = 
-        let a = coefficients.[3] / coefficients.[4]
-        let b = coefficients.[2] / coefficients.[4]
-        let c = coefficients.[1] / coefficients.[4]
-        let d = coefficients.[0] / coefficients.[4]
-        let sq_a = a**2.0
-        let p = -3.0 / 8.0 * sq_a + b
-        let q = 1.0 / 8.0 * sq_a - 1.0 / 2.0 * a * b + c
-        let r = -3.0 / 256.0 * sq_a * sq_a + 1.0 / 16.0 * sq_a * b - 1.0 / 4.0 * a * c + d
-        let s = 
-            if areEqualFloat r 0.0 then
-                let coeffs = [q;p;0.0;1.0;]
-                let s = solve3 coeffs
-                [s.[0]]
-            else
-                let coeffs = [
-                    1.0 / 2.0 * r * p - 1.0 / 8.0 * q * q;
-                    -r;
-                    - 1.0 / 2.0 * p;
-                    1.0;
-                ]
-                let s = solve3 coeffs
-                let z = s.[0]
-                let u = z * z - r
-                let v = 2.0 * z - p
-                let func x =
-                    if areEqualFloat x 0.0 then
-                        0.0
-                    else
-                        if x > 0.0 then
-                            Math.Sqrt(x)
-                        else
-                            0.0                
-                let u = func u
-                let v = func v
-                let coeffs = [
-                    z - u;
-                    if q < 0.0 then -v else v;
-                    1.0;
-                ]
-                let s1 = solve2 coeffs
-                let coeffs = [
-                    z + u;
-                    if q < 0.0 then v else -v;
-                    1.0;
-                ]
-                let s2 = solve2 coeffs
-                s1 @ s2
-        let sub = 1.0 / 4.0 * a
-        s |> List.map (fun t -> t - sub)
+    let filterRealNumbers values tolerance = 
+        let func (i:Complex) = 
+            if Math.Abs(i.Imaginary) < tolerance then
+                Some i.Real
+            else None
+        values
+        |> List.map func
+        |> List.choose id
 
     let build rmajor rminor =
         let local_normal_at hit shape pt = 
-            let paramSquared = rmajor**2.0 + rminor**2.0
-            let x = pt.x
-            let y = pt.y
-            let z = pt.z
-            let sumSquared = x * x + y * y + z * z
-            let vx = 4.0 * x * (sumSquared - paramSquared)
-            let vy = 4.0 * y * (sumSquared - paramSquared + 2.0 * rmajor**2.0)
-            let vz = 4.0 * z * (sumSquared - paramSquared)
-            (vector vx vy vz).normalize()            
+            let a = 1.0 - (rmajor / Math.Sqrt(pt.x**2.0 + pt.y**2.0))
+            (vector (a*pt.x) (a*pt.y) pt.z).normalize()
         let local_intersect shape trail ray = 
-            let ox = ray.origin.x;
-            let oy = ray.origin.y;
-            let oz = ray.origin.z;
-            let dx = ray.direction.x;
-            let dy = ray.direction.y;
-            let dz = ray.direction.z;
-            let sum_d_sqrd = dx * dx + dy * dy + dz * dz;
-            let e = ox * ox + oy * oy + oz * oz - rmajor**2.0 - rminor**2.0
-            let f = ox * dx + oy * dy + oz * dz;
-            let four_a_sqrd = 4.0 * rmajor**2.0
-            let coeffs = [
-                e * e - four_a_sqrd * (rminor**2.0 - oy * oy);
-                4.0 * f * e + 2.0 * four_a_sqrd * oy * dy;
-                2.0 * sum_d_sqrd * e + 4.0 * f * f + four_a_sqrd * dy * dy;
-                4.0 * sum_d_sqrd * f;
-                sum_d_sqrd * sum_d_sqrd;
-            ]
-            let solution = solve4 coeffs
+            let T = 4.0 * rmajor * rmajor
+            let J = ray.direction.magnitude()
+            let K = 2.0 * ray.origin.dotProduct ray.direction
+            let L = ray.origin.magnitude() + rmajor*rmajor - rminor*rminor
+            let G = T * (ray.direction.x**2.0 + ray.direction.y**2.0)
+            let H = 2.0 * T * (ray.origin.x*ray.direction.x + ray.origin.y*ray.direction.y)
+            let I = T * (ray.origin.x**2.0 + ray.origin.y**2.0)
+
+            let a = new Complex(J*J,0.0)
+            let b = new Complex(2.0*J*K, 0.0)
+            let c = new Complex(2.0*J*L + K*K - G, 0.0)
+            let d = new Complex(2.0*K*L - H, 0.0)
+            let e = new Complex(L*L - I, 0.0)
+
+            let solution = solveQuartic a b c d e
             if List.isEmpty solution then
                 []
             else
-                //let intersections = solution |> List.map (fun t -> Shapes.build_intersection t shape trail)
-                //sort_intersection intersections
-                
-                let func mint t = 
-                    if t > epsilon && t < mint then t else mint
-                let t = List.fold func Double.PositiveInfinity solution
-                if Double.IsInfinity(t) then
-                    []
-                else
-                    [Shapes.build_intersection t shape trail]
-                
+                let SURFACE_TOLERANCE = 1.0e-4;
+                let realRoots = filterRealNumbers solution epsilon
+                realRoots
+                |> List.filter (fun i -> i > SURFACE_TOLERANCE)
+                |> List.map (fun t -> Shapes.build_intersection t shape trail)
+                |> sort_intersection
         let bounds_of shape = BoundingBoxes.build_default
         build (Torus(rmajor,rminor)) local_intersect local_normal_at bounds_of
 
