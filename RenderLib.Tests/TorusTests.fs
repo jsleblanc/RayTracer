5aa90405e31bb972fd52a8a4d731ef4de20dacea
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2019-12-15 21:35:52 -0800

    added intersection tests

--- RenderLib.Tests/TorusTests.fs
+++ RenderLib.Tests/TorusTests.fs
@@ -28,13 +28,6 @@ module TorusTests =
         Assert.True(known |> List.exists (fun k -> hasRoot k found))        
 
     [<Fact>]
-    let ``a torus``() =
-        let a = new Complex(-2.5,0.0)
-        let b = new Complex(-2.500000000000039, 1.481838361948336E-15)
-        let r1 = areEqualComplex a b
-        Assert.True(r1)
-
-    [<Fact>]
     let ``Test known quadratic roots``() =
         let test M K L =
            let a = M;
@@ -92,3 +85,64 @@ module TorusTests =
         test (cpx 1.0) (cpx 3.2) (cpx -2.5) (cpx 53.0) (cpx -8.7)
         test (new Complex(-2.3,+4.8)) (new Complex(+3.2,-4.1)) (new Complex(-2.5,+7.7)) (new Complex(53.0,-23.9)) (new Complex(-9.2,-8.7))
 
+    //When t is greater than 0, the point is located in front of the ray's origin (looking down the ray's direction), when t is equal to 0, the point coincides with the ray's origin (O), and when t is negative the point is located behind its origin.
+
+    [<Fact>]
+    let ``A ray misses a torus``() =
+        let t = ShapeTorus.build 4.0 1.0
+        let ray = {
+            origin = point -2.5 -3.0 0.0;
+            direction = vector 1.0 0.0 0.0;
+        }
+        let xs = intersect t [] ray
+        Assert.Equal(0, List.length xs)
+
+    [<Fact>]
+    let ``A ray intersects a torus once``() =
+        let t = ShapeTorus.build 4.0 1.0
+        let ray = {
+            origin = point -2.5 -2.0 0.0;
+            direction = vector 1.0 0.0 0.0;
+        }
+        let xs = intersect t [] ray
+        Assert.Equal(1, List.length xs)
+        Assert.Equal(2.5, xs.[0].t)
+
+    [<Fact>]
+    let ``A ray intersects a torus twice``() =
+        let t = ShapeTorus.build 4.0 1.0
+        let ray = {
+            origin = point -2.5 -1.5 0.0;
+            direction = vector 1.0 0.0 0.0;
+        }
+        let xs = intersect t [] ray
+        Assert.Equal(2, List.length xs)
+        Assert.Equal(1.177, xs.[0].t)
+        Assert.Equal(3.823, xs.[1].t)
+
+    [<Fact>]
+    let ``A ray intersects a torus three times``() =
+        let t = ShapeTorus.build 4.0 1.0
+        let ray = {
+            origin = point -2.5 -1.0 0.0;
+            direction = vector 1.0 0.0 0.0;
+        }
+        let xs = intersect t [] ray
+        Assert.Equal(3, List.length xs)
+        Assert.Equal(0.768, xs.[0].t)
+        Assert.Equal(2.5, xs.[1].t)
+        Assert.Equal(3.268, xs.[2].t)
+
+    [<Fact>]
+    let ``A ray intersects a torus four times``() =
+        let t = ShapeTorus.build 4.0 1.0
+        let ray = {
+            origin = point -2.5 0.0 0.0;
+            direction = vector 1.0 0.0 0.0;
+        }
+        let xs = intersect t [] ray
+        Assert.Equal(4, List.length xs)
+        Assert.Equal(0.5, xs.[0].t)
+        Assert.Equal(1.5, xs.[1].t)
+        Assert.Equal(2.5, xs.[2].t)
+        Assert.Equal(4.5, xs.[3].t)
